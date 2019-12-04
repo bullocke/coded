e05bc5c9efc99181cc35e778d0fe9326eb25cfcf
Eric Bullock <bullocke@bu.edu>
2019-12-03 21:47:30 -0500

    add support for md

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,16 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
+
 # The suffix of source filenames.
-source_suffix = '.rst'
+#source_suffix = '.rst'
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
